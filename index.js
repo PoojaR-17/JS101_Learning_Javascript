da1ff99169ffb0871742b1d6fbab35a9dfcade93
Pooja <poojapuj87@gmail.com>
2024-03-04 07:47:16 +0530

    pushing day1 code to the new javascript repository

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// Using Var
+var a;        
+a = 10;         
+console.log(a); 
+
+var a = 10;
+console.log(a);
+
+
+
+var name = "Pooja";
+console.log(name);
+
+
+
+var x = 12;
+console.log(typeof(x)); // Output: number
+
+var y = "12";
+console.log(typeof(y)); // Output: string
+
